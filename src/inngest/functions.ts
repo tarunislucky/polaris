63fa79db79bee3977e5100565079c86834f84db9
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2026-04-17 17:04:46 +0530

    changed ai to google

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,6 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
-import { anthropic } from "@ai-sdk/anthropic";
+import { google } from "@ai-sdk/google";
 import { firecrawl } from "@/lib/firecrawl";
 
 const URL_REGEX = /https?:\/\/[^\s]+/g;
@@ -34,7 +34,7 @@ export const demoGenerate = inngest.createFunction(
 
     await step.run("generate-text", async () => {
       return await generateText({
-        model: anthropic('claude-3-haiku-20240307'),
+        model: google(process.env.NEXT_PUBLIC_GOOGLE_AI_MODEL!),
         prompt: finalPrompt,
       });
     })
